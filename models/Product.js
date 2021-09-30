542af17415d5185ba66662243d5649314bf52869
jb-laurans <jblaurans@netysoft.com>
2021-10-01 00:04:11 +0200

    add Datas pour Mongo

--- models/Product.js
+++ models/Product.js
@@ -8,9 +8,8 @@ const productSchema = new mongoose.Schema(
     price: { type: Number, required: true },
     brand: { type: String, required: true },
     rating: { type: Number, required: true, default: 0 },
-    numReviews: { type: Number, required: true, dafault: 0 },
-    countInStock: { type: Number, required: true },
-    default: 0,
+    numReviews: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
     description: { type: String, required: true },
   },
   {
